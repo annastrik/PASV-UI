d3cc28d0024a6dbd0adbc79e51da1131a689e55b
Anna Strik <anna.strik1@gmail.com>
2020-11-12 13:12:46 -0800

    test test test

--- modules/cards/cardsTraining/cardsTraining.spec.js
+++ modules/cards/cardsTraining/cardsTraining.spec.js
@@ -5,6 +5,8 @@ import LoginPage from '../../user/_page/LoginPage';
 import { student } from '../../user/_data/user.data';
 import CardsTrainingPage from '../_page/CardsTrainingPage';
 
+// test test test
+
 let nrOfCards;
 
 describe('CARDS TRAINING', () => {
